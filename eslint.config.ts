571aa2aad804eeabfc58a4bf32f2fe6911e2def9
TymianekPL <tymi@tymi.org>
2026-05-28 14:14:49 +0200

    Initial commit
    Signed-off-by: TymianekPL <tymi@tymi.org>

---
+++ eslint.config.ts
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+     {
+          files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+          plugins: {
+               js
+          },
+          extends: ["js/recommended"],
+          languageOptions: {
+               globals: globals.node
+          },
+          rules: {
+               "indent": ["error", 5],
+               "quotes": ["error", "double"],
+               "semi": ["error", "always"]
+          }
+     },
+
+     {
+          files: ["**/*.{ts,mts,cts}"],
+          languageOptions: {
+               globals: globals.node
+          },
+          plugins: {
+               "@typescript-eslint": tseslint.plugin
+          },
+          extends: [
+               ...tseslint.configs.recommended
+          ],
+          rules: {
+               "indent": ["error", 5],
+               "quotes": ["error", "double"],
+               "semi": ["error", "always"],
+               "@typescript-eslint/no-explicit-any": "error",
+               "@typescript-eslint/no-unused-vars": "error",
+               "@typescript-eslint/consistent-type-imports": "error"
+          }
+     }
+]);
