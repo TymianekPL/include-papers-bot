f8880b484ead2ea7d186e7d0234aabadc11c9717
TymianekPL <tymi@tymi.org>
2026-05-28 15:32:46 +0200

    Respond with paper numbers [not ready, has to query the database]
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- src/events/client/messageCreate.ts
+++ src/events/client/messageCreate.ts
@@ -4,6 +4,19 @@ export default {
      execute: async (message) => {
           if (message.author.bot) return;
           let content = message.content.trim();
+
+          const papers = content.match(/\[[PN]\d\d\d\d(R\d+)?\]/gi);
+          if (papers) {
+               const built = papers.reduce(
+                    (built, paper, index) =>
+                         `${built}${
+                              index === 0 ? "" : ","
+                         } [${paper}](<https://wg21.link/${paper.toUpperCase().substring(1, paper.length - 1)}>)`,
+                    "",
+               );
+               await message.reply(built);
+          }
+
           if (!content.startsWith(`<@${process.env.CLIENT_ID}>`)) return;
           content = content
                .substring(`<@${process.env.CLIENT_ID}>`.length)
